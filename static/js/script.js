5552ba132b7217f9f845ff8b4563c6a40c249916
Olesya <progafox@gmail.com>
2021-10-19 18:19:45 +0500

    Add js

---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+// Получить модальный
+var modal = document.getElementById("myModal");
+
+// Получить кнопку, которая открывает модальный
+var btn = document.getElementById("myBtn");
+
+// Получить элемент <span>, который закрывает модальный
+var closebtn = document.getElementById("close");
+
+// Когда пользователь нажимает на кнопку, откройте модальный
+btn.onclick = function() {
+  modal.style.display = "flex";
+}
+
+// Когда пользователь нажимает на <span> (x), закройте модальное окно
+closebtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Когда пользователь щелкает в любом месте за пределами модального, закройте его
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
