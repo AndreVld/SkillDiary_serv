07ce030bbde01ec9db68528988a1c72ef416f275
AndreyD <nutaru@gmail.com>
2021-10-19 16:38:20 +0000

    change models

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from users_app.models import Person
+from users_app.models import Person, City
 
 admin.site.register(Person)
+admin.site.register(City)
