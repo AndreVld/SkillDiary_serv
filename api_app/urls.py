e5f022022b53b4bbb4b6b8b4b8157ef165fcd265
AndreyD <nutaru@gmail.com>
2021-10-24 17:15:45 +0000

    modify api

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework import routers
 
 from api_app import views
+from api_app.views import CourseList
 
 router = routers.DefaultRouter()
 router.register(r'persons', views.PersonViewSet)
@@ -14,7 +15,8 @@ router.register(r'professions', views.ProfessionViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    re_path('^users/(?P<username>.+)/$', CourseList.as_view()),
 
 
 ]
