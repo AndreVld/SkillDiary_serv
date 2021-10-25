45fb64f088ea73cae4f2c95874cb090e9772fe7f
Roman <shmurygin.roman@gmail.com>
2021-10-25 01:20:47 -0700

    GB-27: Course add

--- courseapp/urls.py
+++ courseapp/urls.py
@@ -6,8 +6,8 @@ from courseapp.apps import CourseappConfig
 app_name = CourseappConfig.name
 
 urlpatterns = [
-    path("courses/", courseapp.CourseList.as_view()),
-    #path("add_course/", courseapp.add_course),
+    path("courses/", courseapp.CourseList.as_view(), name='course_list'),
+    path("courses/add/", courseapp.course_add, name='course_add'),
     #re_path(r"^$", courseapp.CourseList.as_view(), name="courses_list"),
     #re_path(r"^read/(?P<pk>\d+)/$", courseapp.CourseRead.as_view(), name="course_read"),
 ]
--- courseapp/views.py
+++ courseapp/views.py
@@ -1,9 +1,8 @@
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.views.generic.detail import DetailView
 from courseapp.models import Course
-
-
-
+from courseapp.forms import AddCourseForm
+from django.shortcuts import render, redirect
 
 
 class CourseList(ListView):
@@ -20,4 +19,36 @@ class CourseRead(DetailView):
         context = super(CourseRead, self).get_context_data(**kwargs)
         context["title"] = "курс/просмотр"
         return context
+"""
+
+
+def course_add(request):
+    if request.method == "POST":
+        form = AddCourseForm(request.POST)
+        if form.is_valid():
+            course = form.save(commit=False)
+            course.person = request.user
+            course.save()
+            #return redirect('course_list')
+    else:
+        form = AddCourseForm()
+    return render(request, 'courseapp/course-add.html', {'form': form})
+
+"""
+class AddCourseView(CreateView):
+    form_class = AddCourseForm
+    template_name = 'courseapp/course-add.html'
+    extra_context = {'title': 'SkillDiary -'}
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddCourseView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(AddCourseView, self).get_context_data()
+        context['current_url'] = resolve(self.request.path_info)
+        return context
+
+    def get_success_url(self):
+        return reverse('tasks:task', kwargs=self.kwargs,)
 """
