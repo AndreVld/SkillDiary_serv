1a35cacb9d8f41a09cf8b6f8dabca524fefa2c3a
AndreyD <nutaru@gmail.com>
2021-10-28 15:30:18 +0000

    change api

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
 from users_app.models import Person
 
-admin.site.register(Person)
+admin.site.register(Person,UserAdmin)
